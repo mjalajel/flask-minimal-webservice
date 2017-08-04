18ce387e2709e028b4f348b82d542c7c025f9f66
Mahmoud Jalajel <mjalajel@ligadata.com>
2017-08-04 23:56:13 +0300

    CHECKPOINT: api runs on default port & config

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python
+from flask import Flask
+
+# Init with default config
+app = Flask(__name__)
+app.config.update(dict(
+    headers={
+        'Access-Control-Allow-Origin': '*',
+        'content-type': 'application/json; charset=utf-8'
+    }
+))
 
 
 def run():
-    print("running...")
+    """
+    Entry point to execute script
+    Forwards call to run web sever
+    """
+    return init_api()
+
+
+def init_api(port=8888, debug=False):
+    """
+    Runs web server
+    """
+    app.logger.info("Starting api with config: " + str(app.config))
+    app.run(host='0.0.0.0', port=port, debug=debug, threaded=True)
 
 if __name__ == '__main__':
     run()
